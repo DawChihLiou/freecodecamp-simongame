da06c831d76f9204f56b66eee0ce4876f20cf9e0
Daw-Chih Liou <dawochih.liou@gmail.com>
2016-12-07 15:04:41 +0800

    setup basic controller component

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import className from 'classnames'
 
-const Controller = () => (
-  <div className="wrap">
-    <div className="row">
-      <div className="col-xs-4 control clickable ">
-        <span className="glyphicon glyphicon-play" aria-hidden="true"></span></div>
-      <div className="col-xs-4 control clickable">
-        <span className="glyphicon glyphicon-off" aria-hidden="true"></span>
-      </div>
-      <div className="col-xs-4 control clickable ">
-        <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+const Controller = ({ onoff=false, start=true, strict=false, onClick = (e) => {console.log(e.target.id)} }) => {
+  var baseStyle   = { 'col-xs-4': true, 'control': true, 'clickable': true }
+  var startStyle  = className(Object.assign({}, baseStyle, { 'active': start }))
+  var switchStyle = className(Object.assign({}, baseStyle, { 'active': onoff }))
+  var strictStyle = className(Object.assign({}, baseStyle, { 'active': strict }))
+
+  return (
+    <div className="wrap" onClick={ onClick }>
+      <div className="row">
+        <div id="start" className={ startStyle }>
+          <span id="start" className="glyphicon glyphicon-play" aria-hidden="true"></span></div>
+        <div id="switch" className={ switchStyle }>
+          <span id="switch" className="glyphicon glyphicon-off" aria-hidden="true"></span>
+        </div>
+        <div id="strict" className={ strictStyle }>
+          <span id="strict" className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Controller
