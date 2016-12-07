6035faa3bc260529e7918410d1aa3b4a9f04186d
Daw-Chih Liou <dawochih.liou@gmail.com>
2016-12-07 12:26:16 +0800

    setup basic layout

---
+++ src/components/Pad.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Pad = () => (
+  <div className="wrap">
+    <div className="row">
+      <div className="col-xs-6 pad clickable p-1"></div>
+      <div className="col-xs-6 pad clickable p-2"></div>
+    </div>
+    <div className="row">
+      <div className="col-xs-6 pad clickable p-3"></div>
+      <div className="col-xs-6 pad clickable p-4"></div>
+    </div>
+  </div>
+)
+
+export default Pad
