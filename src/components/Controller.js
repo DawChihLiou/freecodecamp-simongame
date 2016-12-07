c2983b70f53b934d6b052fc63289d4a885e61785
Daw-Chih Liou <dawochih.liou@gmail.com>
2016-12-07 14:33:11 +0800

    update controller style

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -3,11 +3,14 @@ import React from 'react'
 const Controller = () => (
   <div className="wrap">
     <div className="row">
-      <div className="col-xs-4 control clickable ">Start</div>
+      <div className="col-xs-4 control clickable ">
+        <span className="glyphicon glyphicon-play" aria-hidden="true"></span></div>
       <div className="col-xs-4 control clickable">
         <span className="glyphicon glyphicon-off" aria-hidden="true"></span>
       </div>
-      <div className="col-xs-4 control clickable ">Strict</div>
+      <div className="col-xs-4 control clickable ">
+        <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+      </div>
     </div>
   </div>
 )
