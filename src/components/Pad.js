3624aa790d497457720462a275826453df1ad9a1
Daw-Chih Liou <dawochih.liou@gmail.com>
2016-12-07 15:29:49 +0800

    basic Pad setup

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import classNames from 'classnames'
 
-const Pad = () => (
-  <div className="wrap">
-    <div className="row">
-      <div className="col-xs-6 pad clickable p-1"></div>
-      <div className="col-xs-6 pad clickable p-2"></div>
+const Pad = ({ clickable=true, onMouseDown=(e)=>{console.log('down ', e.target.id)}, onMouseUp=(e)=>{console.log('up ',e.target.id)} }) => {
+  var padStyle = classNames({
+    'col-xs-6': true,
+    'pad': true,
+    'clickable': clickable
+  })
+
+  return (
+    <div
+      className="wrap"
+      onMouseDown={ clickable ? onMouseDown : undefined }
+      onMouseUp={ clickable ? onMouseUp : undefined }
+    >
+      <div className="row">
+        <div id="p-0" className={ padStyle }></div>
+        <div id="p-1" className={ padStyle }></div>
+      </div>
+      <div className="row">
+        <div id="p-2" className={ padStyle }></div>
+        <div id="p-3" className={ padStyle }></div>
+      </div>
     </div>
-    <div className="row">
-      <div className="col-xs-6 pad clickable p-3"></div>
-      <div className="col-xs-6 pad clickable p-4"></div>
-    </div>
-  </div>
-)
+  )
+}
 
 export default Pad
